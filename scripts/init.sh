af95bfb73883185ee638233de74e56963dbd8001
Jason Nam <contact@jasonnam.com>
2019-04-18 15:26:55 +0700

    Update drivers

--- scripts/init.sh
+++ scripts/init.sh
@@ -51,5 +51,5 @@ fi
 
 curl -L "$BASE_URL/binary/0.1.1/scripts/init.sh" | sh -s "$INSTALL_PATH" "$DEFAULT_SCHEME" "$DEFAULT_CACHE_PATH"
 curl -L "$BASE_URL/swift/0.1.1/scripts/init.sh" | sh -s "$INSTALL_PATH" "$DEFAULT_SCHEME" "$DEFAULT_CACHE_PATH"
-curl -L "$BASE_URL/ruby/0.1.1/scripts/init.sh" | sh -s "$INSTALL_PATH" "$DEFAULT_SCHEME" "$DEFAULT_CACHE_PATH"
-curl -L "$BASE_URL/node/0.1.1/scripts/init.sh" | sh -s "$INSTALL_PATH" "$DEFAULT_SCHEME" "$DEFAULT_CACHE_PATH"
+curl -L "$BASE_URL/ruby/0.1.2/scripts/init.sh" | sh -s "$INSTALL_PATH" "$DEFAULT_SCHEME" "$DEFAULT_CACHE_PATH"
+curl -L "$BASE_URL/node/0.1.2/scripts/init.sh" | sh -s "$INSTALL_PATH" "$DEFAULT_SCHEME" "$DEFAULT_CACHE_PATH"
